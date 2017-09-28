797f2f72fe0aaf196852813376ccac695937fa72
xSamagon <>
2017-09-29 01:04:58 +0200

    Added PuushProxy
    Puush ProxyServer instead of hosts file :)

---
+++ PuushInitializer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PuushInitializer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Puush-Server IP:";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(113, 15);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(183, 20);
+            this.textBoxServer.TabIndex = 1;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(113, 41);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(183, 20);
+            this.textBoxPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puush-Server Port:";
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(14, 70);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(130, 23);
+            this.btnSet.TabIndex = 4;
+            this.btnSet.Text = "Set own server";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(166, 70);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(130, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove own server";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 105);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PuushInitializer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ PuushInitializer/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuushInitializer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                StreamReader sr = new StreamReader(Environment.ExpandEnvironmentVariables(@"%AppData%\puush\puush.ini"));
+                while (sr.Peek() >= 0)
+                {
+                    string txt = sr.ReadLine();
+                    if (txt.StartsWith("ProxyServer"))
+                    {
+                        txt = "ProxyServer = " + textBoxServer.Text;
+                    }
+                    else if (txt.StartsWith("ProxyPort"))
+                    {
+                        txt = "ProxyPort = " + textBoxPort.Text;
+                    }
+                    lines.Add(txt);
+                }
+                sr.Close();
+                StreamWriter sw = new StreamWriter(Environment.ExpandEnvironmentVariables(@"%AppData%\puush\puush.ini"), false);
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line);
+                }
+                sw.Close();
+                MessageBox.Show("Setted!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't open %appdata%/puush.ini! Error: " + ex.Message);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                StreamReader sr = new StreamReader(Environment.ExpandEnvironmentVariables(@"%AppData%\puush\puush.ini"));
+                while (sr.Peek() >= 0)
+                {
+                    string txt = sr.ReadLine();
+                    if (txt.StartsWith("ProxyServer"))
+                    {
+                        txt = "ProxyServer = ";
+                    }
+                    else if (txt.StartsWith("ProxyPort"))
+                    {
+                        txt = "ProxyPort = ";
+                    }
+                    lines.Add(txt);
+                }
+                sr.Close();
+                StreamWriter sw = new StreamWriter(Environment.ExpandEnvironmentVariables(@"%AppData%\puush\puush.ini"), false);
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line);
+                }
+                sw.Close();
+                MessageBox.Show("Removed!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't open %appdata%/puush.ini! Error: " + ex.Message);
+            }
+        }
+    }
+}
